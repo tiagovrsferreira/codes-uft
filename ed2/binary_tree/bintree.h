a316a66209cefbf89c1bd0191fbb2ebc9c714d5f
Tiago Veras <tiago.veras@mail.uft.edu.br>
2025-04-14 22:28:14 -0300

    init commit

---
+++ ed2/binary_tree/bintree.h
@@ -0,0 +1,22 @@
+#ifndef BINTREE_H
+#define BINTREE_H
+#include <stdbool.h>
+
+typedef struct _no {
+    int key;
+    struct _no *left, *right, *parent;
+}TNo;
+
+typedef struct {
+    TNo* root;
+}BinTree;
+
+BinTree* BinTree_create();
+bool BinTree_insert(BinTree*, int);
+TNo* BinTree_search(Tno*, int);
+TNo* BinTree_search_r(TNo*, int);
+void BinTree_pre(TNo*);
+void BinTree_in(TNo*);
+void BinTree_pos(TNo*);
+
+#endif
